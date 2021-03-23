390ef18899af67386c58dd93d6e607c24f78fafd
Basil Khwaja <khwajabasil@gmail.com>
2021-03-23 16:09:22 -0700

    Basic setup

---
+++ app/constants/images.js
@@ -0,0 +1,22 @@
+const ratOnBike = require('../assets/images/recumbentrat1.png');
+const basicRat = require('../assets/images/SORat2.png');
+const triangleRat = require('../assets/images/SORat3.png');
+const pentagonRat = require('../assets/images/SORat4.png');
+const cubeRat = require('../assets/images/SORat5.png');
+const sphereRat = require('../assets/images/SORat6.png');
+const susRat = require('../assets/images/susrate.png');
+const tandemRats = require('../assets/images/tandemrate.png');
+const workoutRat = require('../assets/images/workoutrate.png');
+
+//work in progress
+export default {
+    ratOnBike,
+    basicRat,
+    triangleRat,
+    pentagonRat,
+    cubeRat,
+    sphereRat,
+    susRat,
+    tandemRats,
+    workoutRat
+}
